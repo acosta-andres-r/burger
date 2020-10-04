ff2108e77a29571064adecbe9ab8ec7f637291ac
Andres A <acosta.andres.r@gmail.com>
2020-10-03 23:44:47 -0500

    Created seeds for database.

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Burger Barn', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Smokin' Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
