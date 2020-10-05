1ecb2ecf92c3bfd63788060946eb196ab90e82ed
Andres A <acosta.andres.r@gmail.com>
2020-10-05 13:01:14 -0500

    Added update method into ORM with helper function.

--- config/orm.js
+++ config/orm.js
@@ -15,6 +15,28 @@ function printQuestionMarks(num) {
   return arr.toString();
 }
 
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    var value = ob[key];
+
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
 // OBJECT RELATIONAL MAPPER for all our SQL statement functions
 const orm = {
   selectAll: function (tableInput, cb) {
@@ -38,6 +60,21 @@ const orm = {
     console.log(queryString);
 
     connection.query(queryString, vals, function (err, result) {
+      if (err) throw err;
+
+      cb(result);
+    });
+  },
+  updateOne: function (table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
       if (err) throw err;
 
       cb(result);
