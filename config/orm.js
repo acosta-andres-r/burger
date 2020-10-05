ad73276a399eba18b06171a0604c7b8a669acdac
Andres A <acosta.andres.r@gmail.com>
2020-10-05 13:00:02 -0500

    Added insert method into ORM.

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,20 @@
 const connection = require("./connection");
 
+// HELPER FUNCTION for SQL SYNTAX
+
+// The above helper function loops through and 
+// creates an array of question marks - ["?", "?", "?"] - 
+// and turns it into a string.
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
 // OBJECT RELATIONAL MAPPER for all our SQL statement functions
 const orm = {
   selectAll: function (tableInput, cb) {
@@ -9,6 +24,24 @@ const orm = {
 
       cb(result);
     })
+  },
+  insertOne: function (table, cols, vals, cb) {
+    let queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ");";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function (err, result) {
+      if (err) throw err;
+
+      cb(result);
+    });
   },
 }
 
